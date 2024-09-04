ce745f85e8025944c4d059138909b48fc97e01cb
AlibardaWasTaken <folde123111@gmail.com>
2024-09-04 23:34:44 +0300

    BloodFoot fix

--- ENSYSCore.cs
+++ ENSYSCore.cs
@@ -57,6 +57,95 @@ namespace ENSYS
 
         }
 
+        [HarmonyPatch]
+        public class BloodFootPatcher
+        {
+            private static readonly ContactPoint2D[] contactBuffer = new ContactPoint2D[8];
+            [HarmonyPatch(typeof(LimbBehaviour), "OnCollisionEnter2D")]
+            [HarmonyPrefix]
+            public static bool Prefix(LimbBehaviour __instance, Collision2D collision)
+            {
+                if (DurabilitySystemsUsers.ContainsKey(__instance.transform.root))
+                {
+
+                    if (DurabilitySystemsUsers[__instance.transform.root].shouldPatchFootBlood == true)
+                    {
+                        int contacts = collision.GetContacts(contactBuffer);
+                        float num = Utils.GetAverageImpulseRemoveOutliers(contactBuffer, contacts, 1f) / 1f * UserPreferenceManager.Current.FragilityMultiplier * DurabilitySystemsUsers[__instance.transform.root].FootBloodMult;
+                        Vector2 normal = contactBuffer[0].normal;
+                        Vector2 point = contactBuffer[0].point;
+                        PhysicalBehaviour physicalBehaviour;
+        
+                        if (Global.main.PhysicalObjectsInWorldByTransform.TryGetValue(collision.transform, out physicalBehaviour) && physicalBehaviour.SimulateTemperature && physicalBehaviour.Temperature >= 70f)
+                        {
+                            __instance.Damage(physicalBehaviour.Temperature / 140f);
+                            __instance.SkinMaterialHandler.AddDamagePoint(DamageType.Burn, point, physicalBehaviour.Temperature * 0.01f);
+                            if (__instance.NodeBehaviour.IsConnectedToRoot && !__instance.IsParalysed)
+                            {
+                                __instance.Person.AddPain(1f);
+                            }
+                            __instance.Wince(150f);
+                            if (physicalBehaviour.Temperature >= 100f)
+                            {
+                                __instance.CirculationBehaviour.HealBleeding();
+                            }
+                        }
+                        if (__instance.HasBrain && num > 0.6f && (double)UnityEngine.Random.value > 0.8)
+                        {
+                            __instance.CirculationBehaviour.InternalBleedingIntensity += num;
+                        }
+                        if (num < 2f)
+                        {
+                            return false;
+                        }
+                        __instance.BruiseCount += 1;
+                        PropagateImpactDamage(num, normal, point, 0, __instance, DurabilitySystemsUsers[__instance.transform.root].appliedDurLmb[__instance]);
+                        if (num < 1f || __instance.IsAndroid || __instance.CirculationBehaviour.GetAmountOfBlood() < 0.2f)
+                        {
+                            return false;
+                        }
+                        if (UnityEngine.Random.value > 0.8f)
+                        {
+                            __instance.CirculationBehaviour.Cut(point, normal);
+                        }
+                        if (num < 3f && __instance.Health > __instance.InitialHealth * 0.2f)
+                        {
+                            return false;
+                        }
+                        __instance.PhysicalBehaviour.CreateImpactEffect(point, normal, Mathf.Clamp(num / 4f, 1f, 2f));
+                        collision.gameObject.SendMessage("Decal", new DecalInstruction(__instance.BloodDecal, point, __instance.CirculationBehaviour.GetComputedColor(__instance.GetOriginalBloodType().Color), 1f), SendMessageOptions.DontRequireReceiver);
+                        return false;
+                    }
+ 
+                }
+                return true;
+            }
+
+            private static void PropagateImpactDamage(float impulse, Vector2 direction, Vector2 pos, int iteration, LimbBehaviour origin, DurabilityBaseLimb calledLimb)
+            {
+                calledLimb.ActOnImpact(impulse, pos);
+                if (iteration >= 8)
+                {
+                    return;
+                }
+                for (int i = 0; i < calledLimb.limb.ConnectedLimbs.Count; i++)
+                {
+                    LimbBehaviour limbBehaviour = calledLimb.limb.ConnectedLimbs[i];
+                    if (!(limbBehaviour == origin))
+                    {
+                        float num = Vector2.Dot((calledLimb.transform.position - limbBehaviour.transform.position).normalized, direction);
+                        if (num > 0f)
+                        {
+                            PropagateImpactDamage(num * impulse * 0.9f, direction, pos, iteration + 1, calledLimb.limb, calledLimb);
+                        }
+                    }
+                }
+            }
+
+
+        }
+
+
 
         [HarmonyPatch]
         public class CrushPatcher
@@ -194,7 +283,9 @@ namespace ENSYS
 
     public class DurabilityTagged : MonoBehaviour
     {
-      public  PersonBehaviour person;
+        public bool shouldPatchFootBlood = true;
+        public float FootBloodMult = 0.1f;
+        public  PersonBehaviour person;
         private void Awake()
         {
             if (this.TryGetComponent<PersonBehaviour>(out person) == false)
@@ -469,14 +560,6 @@ namespace ENSYS
         public abstract bool Take(int amount);
         public abstract void Push(int amount);
     }
-
-
-
-
-
-
-
-
 
 
 
