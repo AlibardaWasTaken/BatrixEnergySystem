62788be13e8aa17c9d399a6083c830c8d1c30ad2
AlibardaWasTaken <folde123111@gmail.com>
2024-09-04 23:51:15 +0300

    added basepower and basepassive

--- ENSYSCore.cs
+++ ENSYSCore.cs
@@ -561,6 +561,59 @@ namespace ENSYS
         public abstract void Push(int amount);
     }
 
+    public interface INullable
+    {
+        void Nullify();
+    }
+
+    public interface IDescriptable
+    {
+        string DescTitle();
+
+        string Description();
+
+        Sprite DescImage();
+    }
+
+    public abstract class BasePower : MonoBehaviour, INullable, IDescriptable
+    {
+        public abstract Sprite DescImage();
+        public abstract string Description();
+        public abstract string DescTitle();
+        public abstract void Nullify();
+    }
+
+
+    public abstract class BasePassive : MonoBehaviour, INullable, IDescriptable
+    {
+        public bool CanBeDeleted = true;
+        public bool IsEnabled = true;
+        public bool IsLocked = false;
+
+        public bool CanBeDisabled = true;
+        public bool DisplaysInMenu = true;
+
+        public void Switch()
+        {
+            IsEnabled = !IsEnabled;
+            OnSwitch();
+        }
+
+        public virtual void OnSwitch()
+        {
+        }
+
+        public abstract Sprite DescImage();
+        public abstract string Description();
+        public abstract string DescTitle();
+        public virtual void Nullify()
+        {
+
+        }
+    }
+
+
+
 
 
     public class EnergySystem : AbstractEnergySystem
